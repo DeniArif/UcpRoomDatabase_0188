36dcced09de820b7acf0b2aa19ff5c80db55919d
DeniArif <deniarifwibowo82@gmail.com>
2024-12-21 15:17:37 +0700

    Selesai Pada Pembuatan KrsDatabaseMatakuliah.kt

--- app/src/main/java/com/example/ucp2/database/KrsDatabaseMatakuliah.kt
+++ app/src/main/java/com/example/ucp2/database/KrsDatabaseMatakuliah.kt
@@ -1,10 +1,31 @@
 package com.example.ucp2.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.ucp2.data.dao.MatakuliahDao
 import com.example.ucp2.entity.Matakuliah
 
 @Database(entities = [Matakuliah::class], version = 1, exportSchema = false)
 abstract class KrsDatabaseMatakuliah : RoomDatabase(){
+
+    abstract fun matakuliahDao(): MatakuliahDao
+
+    companion object {
+        @Volatile
+        private  var Instance: KrsDatabaseMatakuliah? = null
+
+        fun getDatabase(context: Context): KrsDatabaseMatakuliah {
+            return  Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    KrsDatabaseMatakuliah::class.java,
+                    "KrsDatabaseMatakuliah"
+                ).build().also { Instance = it }
+
+            }
+        }
+    }
 
 }
