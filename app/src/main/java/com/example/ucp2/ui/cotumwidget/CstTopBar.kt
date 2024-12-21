84cf136ce1615b36f0a9204c292c91e165ad7ec0
DeniArif <deniarifwibowo82@gmail.com>
2024-12-22 01:03:08 +0700

    Melanjutkan kode di CstTopBar.kt

--- app/src/main/java/com/example/ucp2/ui/cotumwidget/CstTopBar.kt
+++ app/src/main/java/com/example/ucp2/ui/cotumwidget/CstTopBar.kt
@@ -2,13 +2,17 @@ package com.example.ucp2.ui.cotumwidget
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TopAppbar(
@@ -31,8 +35,18 @@ fun TopAppbar(
                 TextButton(
                     onClick = onBack,
                     modifier = Modifier.align(Alignment.CenterVertically)
-                ) { }
+                ) {
+                    Text("Kembali")
+                }
+                Spacer(modifier = Modifier.weight(2f))
             }
+
+            Text(
+                text = judul,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.Center)
+            )
         }
     }
 }
