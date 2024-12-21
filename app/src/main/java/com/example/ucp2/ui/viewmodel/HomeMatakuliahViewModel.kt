2355531f58b98f60c32c32d8333139c360c010e3
DeniArif <deniarifwibowo82@gmail.com>
2024-12-22 00:39:21 +0700

    Selesai(KalauTidakError) di penambahan code HomeMatakuliahViewModel.kt

--- app/src/main/java/com/example/ucp2/ui/viewmodel/HomeMatakuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/HomeMatakuliahViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.ucp2.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.entity.Matakuliah
 import com.example.ucp2.repository.RepositoryMatkul
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMatakuliahViewModel(
     private val repositoryMatkul: RepositoryMatkul
@@ -26,6 +30,22 @@ class HomeMatakuliahViewModel(
             emit(HomeUiState(isLoading = true))
             delay(900)
         }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = false,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
 }
 
 data class HomeUiState(
