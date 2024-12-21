eaf6b37f459be35d95805c4f22367ed758f7ac77
DeniArif <deniarifwibowo82@gmail.com>
2024-12-22 00:25:18 +0700

    Menambahkan kode di HomeMatakuliahView.kt fungsinya

--- app/src/main/java/com/example/ucp2/ui/viewmatakuliah/HomeMatakuliahView.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmatakuliah/HomeMatakuliahView.kt
@@ -1,14 +1,25 @@
 package com.example.ucp2.ui.viewmatakuliah
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.entity.Matakuliah
 import com.example.ucp2.repository.RepositoryMatkul
@@ -51,18 +62,42 @@ fun ListMatakuliah(
 fun CardsMatkul (
     matkul: Matakuliah,
     modifier: Modifier = Modifier,
-    onClick: (String) -> Unit,
+    onClick: (String) -> Unit = { },
 ){
     Card (
         onClick = onClick,
         modifier = modifier
             .fillMaxWidth()
-            .padding()
+            .padding(8.dp)
     ){
         Column (
             modifier = Modifier.padding(8.dp)
         ){
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Person, contentDescription = " ")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = matkul.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment =  Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = " ")
+                Spacer(modifier = Modifier.padding(4.dp))
 
+                Text(
+                    text = matkul.kode,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
         }
     }
 }
