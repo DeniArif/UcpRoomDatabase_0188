872995b823b481b5454242213338120b665feaf7
DeniArif <deniarifwibowo82@gmail.com>
2024-12-21 15:26:55 +0700

    membuat UpdateMatakuliahView.kt

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/UpdateMatakuliahViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.ucp2.repository.RepositoryMatkul
+
+class UpdateMatakuliahViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMatkul: RepositoryMatkul
+): ViewModel() {
+    var
+}
