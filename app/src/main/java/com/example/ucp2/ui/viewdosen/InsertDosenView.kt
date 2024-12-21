aaa333f1a68be8aa974b917633d1bc310d431ce3
DeniArif <deniarifwibowo82@gmail.com>
2024-12-21 09:14:55 +0700

    Membuat file InsertDosenView.kt dan isi kode untuk mengelola data input

---
+++ app/src/main/java/com/example/ucp2/ui/viewdosen/InsertDosenView.kt
@@ -0,0 +1,128 @@
+package com.example.ucp2.ui.viewdosen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.navigation.AlamatNavigasi
+import com.example.ucp2.ui.viewmodel.DosenEvent
+import com.example.ucp2.ui.viewmodel.DosenViewModel
+import com.example.ucp2.ui.viewmodel.FormErrorState
+import com.example.ucp2.ui.viewmodel.PenyediaDosenViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiInsert : AlamatNavigasi {
+    override val route : String = "Insert_dosen"
+}
+
+@Composable
+fun InsertDosenView(
+    onBack: () -> Unit,
+    onNavigasi: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DosenViewModel = viewModel(factory = PenyediaDosenViewModel.Factory)
+){
+    val uiState = viewModel.uiState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val corutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            corutineScope.launch {
+                snackbarHostState.showSnackbar(message)
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+}
+
+@Composable
+fun
+
+@Composable
+fun FormDosen(
+    dosenEvent: DosenEvent =DosenEvent(),
+    onValueChange: (DosenEvent) -> Unit,
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+){
+    val jeniskelamin = listOf("Laki - Laki", "Perempuan")
+
+    Column(
+      modifier = modifier.fillMaxWidth()
+    ){
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = dosenEvent.nama,
+            onValueChange = {
+                onValueChange(dosenEvent.copy(nama = it))
+            },
+            label = { Text("nama")},
+            isError = errorState.nama !=null,
+            placeholder = { Text("Masukkan nama")},
+        )
+        Text(
+            text = errorState.nama ?:"",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = dosenEvent.nidn,
+            onValueChange = {
+                onValueChange(dosenEvent.copy(nidn = it))
+            },
+            label = { Text("nama")},
+            isError = errorState.nidn !=null,
+            placeholder = { Text("Masukkan nama")},
+        )
+        Text(
+            text = errorState.nidn ?:"",
+            color = Color.Red
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Jenis Kelamin")
+        Row (
+          modifier = Modifier.fillMaxWidth()
+        ){
+            jeniskelamin.forEach{ jk ->
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ){
+                    RadioButton(
+                        selected = dosenEvent.jeniskelamin == jk,
+                        onClick =  {
+                            onValueChange(dosenEvent.copy(jeniskelamin = jk))
+                        },
+                    )
+                    Text(
+                        text =  jk,
+                    )
+                }
+            }
+        }
+        Text(
+            text = errorState.jeniskelamin ?:"",
+            color = Color.Red
+        )
+
+    }
+}
