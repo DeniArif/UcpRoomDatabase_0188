1f3d69abe51847541d736cbb2a70175277d99bdc
DeniArif <deniarifwibowo82@gmail.com>
2024-12-22 00:08:18 +0700

    Menambahkan kode di HomeMatakuliahView.kt

--- app/src/main/java/com/example/ucp2/ui/viewmatakuliah/HomeMatakuliahView.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmatakuliah/HomeMatakuliahView.kt
@@ -1,14 +1,68 @@
 package com.example.ucp2.ui.viewmatakuliah
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.entity.Matakuliah
+import com.example.ucp2.repository.RepositoryMatkul
 import com.example.ucp2.ui.viewmodel.HomeMatakuliahViewModel
 import com.example.ucp2.ui.viewmodel.PenyediaViewModel
 
 @Composable
 fun HomeMatakuliahView(
     viewModel: HomeMatakuliahViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMatkul: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
 
 ){
 
+}
+
+@Composable
+fun ListMatakuliah(
+  listMatkul: List<Matakuliah>,
+  modifier: Modifier = Modifier,
+  onClick: (String) -> Unit = { }
+){
+    LazyColumn(
+        modifier = modifier
+    ){
+        items(
+            items = listMatkul,
+            itemContent = { matkul ->
+                CardsMatkul (
+                    matkul = matkul,
+                    onClick = { onClick(matkul.kode)}
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun CardsMatkul (
+    matkul: Matakuliah,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit,
+){
+    Card (
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding()
+    ){
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ){
+
+        }
+    }
 }
