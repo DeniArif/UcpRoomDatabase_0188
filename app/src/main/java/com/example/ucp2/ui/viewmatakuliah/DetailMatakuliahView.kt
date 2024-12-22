3cab8c61cbca261e2039973b86595ffa39b1db9c
DeniArif <deniarifwibowo82@gmail.com>
2024-12-22 21:54:20 +0700

    Melanjutkan membuat fungsi di DetailMatakuliahView.kt

--- app/src/main/java/com/example/ucp2/ui/viewmatakuliah/DetailMatakuliahView.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmatakuliah/DetailMatakuliahView.kt
@@ -1,11 +1,24 @@
 package com.example.ucp2.ui.viewmatakuliah
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.entity.Matakuliah
 import com.example.ucp2.ui.viewmodel.MatakuliahViewModel
 import com.example.ucp2.ui.viewmodel.PenyediaViewModel
 
@@ -18,7 +31,60 @@ fun DetailMhsView(
 ){}
 
 @Composable
+fun ComponenDetailMtk(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul :",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya, fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
 
+
+@Composable
+fun ItemDetailMtk(
+    modifier: Modifier =Modifier,
+    matakuliah: Matakuliah
+){
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponenDetailMtk(judul = "Kode", isinya = matakuliah.kode)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponenDetailMtk(judul = "Nama", isinya = matakuliah.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponenDetailMtk(judul = "sks", isinya = matakuliah.sks)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponenDetailMtk(judul = "Semester", isinya = matakuliah.semester)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponenDetailMtk(judul = "Jenis", isinya = matakuliah.jenis)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponenDetailMtk(judul = "Dosen Pengampu", isinya = matakuliah.dosenpengampu)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+}
 @Composable
 private fun DeleteConfirmationDialog(
   onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
