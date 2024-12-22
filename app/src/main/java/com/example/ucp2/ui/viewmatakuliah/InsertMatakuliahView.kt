ef763becd158ebbd548a4f94c7e3d6fd4051ea36
DeniArif <deniarifwibowo82@gmail.com>
2024-12-22 19:23:26 +0700

    Membuat kode di InsertMatakuliahView.kt bagian FormMatakuliah

--- app/src/main/java/com/example/ucp2/ui/viewmatakuliah/InsertMatakuliahView.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmatakuliah/InsertMatakuliahView.kt
@@ -1,2 +1,68 @@
 package com.example.ucp2.ui.viewmatakuliah
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.viewmodel.FormErrorStateMatkul
+import com.example.ucp2.ui.viewmodel.MatakuliahEvent
+import com.example.ucp2.ui.viewmodel.MatakuliahViewModel
+import com.example.ucp2.ui.viewmodel.PenyediaViewModel
+
+@Composable
+fun InsertMatakuliahView(
+  onBack: () -> Unit,
+  onNavigate: () -> Unit,
+  modifier: Modifier = Modifier,
+  viewModel: MatakuliahViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){}
+
+@Composable
+fun FormMatakuliah(
+  matakuliahEvent: MatakuliahEvent = MatakuliahEvent(),
+  onValueChange: (MatakuliahEvent) -> Unit,
+  errorState: FormErrorStateMatkul = FormErrorStateMatkul(),
+  modifier: Modifier = Modifier
+){
+    val semester = listOf("1","2","3","4","5","6","7","8")
+    val sks = listOf("1","2","3")
+
+    Column (
+        modifier = modifier.fillMaxWidth()
+    ){
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = matakuliahEvent.nama,
+            onValueChange = {
+                onValueChange(matakuliahEvent.copy(nama = it))
+            },
+            label = { Text("nama")},
+            isError = errorState.nama !=null,
+            placeholder = { Text("Masukkan nama")}
+        )
+        Text(
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = matakuliahEvent.kode,
+            onValueChange = {
+                onValueChange(matakuliahEvent.copy(kode = it))
+            },
+            label = { Text("kode")},
+            isError = errorState.kode !=null,
+            placeholder = { Text("Masukkan kode")}
+        )
+        Text(
+            text = errorState.kode ?: "",
+            color = Color.Red
+        )
+    }
+
+}
--- app/src/main/java/com/example/ucp2/ui/viewmatakuliah/UpdateMatakuliahView.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmatakuliah/UpdateMatakuliahView.kt
@@ -1,10 +1,20 @@
 package com.example.ucp2.ui.viewmatakuliah
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.ui.viewmodel.PenyediaViewModel
 import com.example.ucp2.ui.viewmodel.UpdateMatakuliahViewModel
+import kotlinx.coroutines.launch
+
 
 @Composable
 fun UpdateMtkView(
@@ -12,4 +22,37 @@ fun UpdateMtkView(
   onNavigate: () -> Unit,
   modifier: Modifier = Modifier,
   viewModel: UpdateMatakuliahViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+){
+  val uiState = viewModel.updateUIState
+  val snackbarHostState = remember { SnackbarHostState() }
+  val corutineScope = rememberCoroutineScope()
+
+  LaunchedEffect(uiState.snackBarMessage) {
+    println("LaunchedEffect triggered")
+    uiState.snackBarMessage?.let { message ->
+      println("Snackbar message received: $message")
+      corutineScope.launch {
+        println("Launching coroutine for snackbar")
+        snackbarHostState.showSnackbar(
+          message = message,
+          duration = SnackbarDuration.Long
+        )
+        viewModel.resetSnackBarMessage()
+      }
+    }
+
+    Scaffold (
+      modifier = modifier,
+      snackbarHost = {SnackbarHostState(hostState = snackbarHostState) },
+      topBar = {
+        TopAppBar(
+          judul = "Edit Matakuliah",
+          showBackButton = true,
+          onBack = onBack,
+        )
+      }
+    )
+
+
+  }
+}
